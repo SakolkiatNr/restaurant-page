9aa1eb99c202ac6a3d2aa890085fab22f29a9558
Sakolkiat <sakolkiat.n@gmail.com>
2025-08-23 12:01:29 +0700

    Edit updateDisplay function

--- src/display.js
+++ src/display.js
@@ -1,20 +1,42 @@
-// import modules
-
-
-// add btn event listener
+// import pages
+import { home } from "./pages/home";
+import { menu } from "./pages/menu";
+import { contact } from "./pages/contact";
 
 export default class ScreenDisplay {
+
+    pages = {
+        home,
+        menu,
+        contact,
+    }
+
+    // remove -btns
+    regex = /^(.*?)-btn$/;
     
     activateNavBtns() {
         const btns = document.querySelector('#nav-btns');
         btns.addEventListener('click', (e) => {
-            console.log(e.target.id);
+            this.updateDisplay();
+            
+            // replace button-btn to button
+            const page = e.target.id.replace(this.regex, '$1');
+            // console.log(page);
+            this.updateDisplay(page);
         })   
     }
 
     clearScreen() {
         const containerTarget = document.querySelector('#content-container')
         containerTarget.innerHTML = '';
+    }
+
+    updateDisplay(page) {
+        this.clearScreen();
+
+        if (page && this.pages[page]) {
+            console.log(this.pages[page]);
+        }
     }
 }
 
